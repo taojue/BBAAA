5d6f2f257238dff88a85138486176c9f2011b7a5
taojue <132348549+taojue@users.noreply.github.com>
2023-05-02 19:27:36 +0800

    Update and rename README.md to fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,14 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print('Time is up!')
+
+if __name__ == '__main__':
+    focus_timer(25)
